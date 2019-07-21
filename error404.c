d2231ab1536fd84d8f5b0a0332559a31c8363627
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-07-21 15:48:30 +0200

    new stuff

--- error404.c
+++ error404.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   flags.c                                            :+:      :+:    :+:   */
+/*   error404.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anben <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/15 15:14:23 by anben             #+#    #+#             */
-/*   Updated: 2019/07/15 15:14:26 by anben            ###   ########.fr       */
+/*   Created: 2019/07/21 15:38:23 by anben             #+#    #+#             */
+/*   Updated: 2019/07/21 15:38:26 by anben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int run_flags(char flag)
+char    flag_404(char *rootname)
 {
-    if (flag == 'l')
-    return (1);
-    if (flag == 'r')
-    return (2);
-    if (flag == 'a')
-    return (3);
-    if (flag == 't')
-    return (4);
-    if (flag == 'R')
-    return (5);
-    return (0);
-}
-unsigned char   scan_flag404(unsigned char *flag)
-{
-    flag_404("-");
-    return ;
+    //flag error handler
 }
