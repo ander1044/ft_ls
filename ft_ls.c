fb752932a730f646d5fb19fec0cc18ddaed1ef38
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-08-31 17:52:55 +0200

    ft_ls

--- ft_ls.c
+++ ft_ls.c
@@ -14,27 +14,45 @@
 
 void	o_dir(const char *content)
 {
-	char			c_path[2000];
-	struct dirent	*d;
-	DIR				*dir; 
+	DIR				*d; 
 	
-	dir = opendir(content);
-	if (!dir)
+	d = opendir(content);
+	if (!d)
 	{
 		ft_putstr("ft_ls:");
 		perror(content);
 		exit(1);
 	}
-	while ((d = readdir(dir)) != NULL)
+	while(1)
+	{
+		const char		*c_path;
+		struct dirent	*entry;
+
+		entry = readdir(d);
+	if(!entry)
+	{
+		break;
+	}
+	c_path = entry->d_name;
+	printf ("%s/%s\n", content, d_name);
+	if (! (entry->d_type & DT_DIR)) 
+	{
+	    printf ("%s/%s\n", content, d_name);
+	}
+	if (entry->d_type & DT_DIR) 
+	//while ((d = readdir(dir)) != NULL)
 	{
 		if(ft_strcmp(d->d_name, ".") != 0 && ft_strcmp(d->d_name, "..")!= 0)
 		{
-			ft_putstr(">>");
+			char path[PATH_MAX];
+			
+			printf ("%s\n", path);
+		/*	ft_putstr(">>");
 			ft_putendl(d->d_name);
 			ft_strcpy(c_path, content);
 			ft_strcat(c_path, "/");
 			ft_strcat(c_path, d->d_name);
-			o_dir(c_path);
+			o_dir(c_path);*/
 		}
 
 	}
@@ -43,9 +61,8 @@ void	o_dir(const char *content)
 
 int	main(int ac, char *av[])
 {
-	char c_path;
+	char *c_path;
 	if(ac == 1)
-
 	o_dir(c_path);
 	return (0);
 }
